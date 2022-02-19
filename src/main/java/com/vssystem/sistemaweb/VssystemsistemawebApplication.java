2080b3180a23a1ca7321639d886c0896b52f9c0a
Vanderson Raimundo da Silva <wandersonwandao@hotmail.com>
2022-02-19 20:30:13 -0300

    Criacao do Projeto

---
+++ src/main/java/com/vssystem/sistemaweb/VssystemsistemawebApplication.java
@@ -0,0 +1,13 @@
+package com.vssystem.sistemaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VssystemsistemawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VssystemsistemawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vssystem/sistemaweb/VssystemsistemawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vssystem.sistemaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VssystemsistemawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
