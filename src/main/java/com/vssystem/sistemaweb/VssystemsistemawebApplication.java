de1bdc5fd40ed876e4fe7e5839b77a3e36102958
Vanderson Raimundo da Silva <wandersonwandao@hotmail.com>
2022-02-20 14:13:28 -0300

    Criando Operacao de Instanciação

--- src/main/java/com/vssystem/sistemaweb/VssystemsistemawebApplication.java
+++ src/main/java/com/vssystem/sistemaweb/VssystemsistemawebApplication.java
@@ -1,13 +1,28 @@
 package com.vssystem.sistemaweb;
 
+import com.vssystem.sistemaweb.model.Categoria;
+import com.vssystem.sistemaweb.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
 @SpringBootApplication
-public class VssystemsistemawebApplication {
+public class VssystemsistemawebApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VssystemsistemawebApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
