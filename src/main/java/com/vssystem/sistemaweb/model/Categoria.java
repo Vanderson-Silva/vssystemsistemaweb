2a097137f5828c5fe4ca906f9a9b5bffc9560433
Vanderson Raimundo da Silva <wandersonwandao@hotmail.com>
2022-02-20 15:31:42 -0300

    Produtos e Associacao Muitos para Muitos

--- src/main/java/com/vssystem/sistemaweb/model/Categoria.java
+++ src/main/java/com/vssystem/sistemaweb/model/Categoria.java
@@ -1,12 +1,11 @@
 package com.vssystem.sistemaweb.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
@@ -18,6 +17,9 @@ private static final long serialVersionUID = 1L;
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<Produto>();
 
 	public Categoria() {
 
@@ -43,6 +45,14 @@ private static final long serialVersionUID = 1L;
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
